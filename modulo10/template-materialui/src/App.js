8e3f4770d60a5b4e0ac039abf85a35c391eba6ae
werikmedis0@gmail.com <werik-medis@hotmail.com>
2024-05-27 23:26:01 -0300

    Atividade descomplica feita

--- modulo10/template-materialui/src/App.js
+++ modulo10/template-materialui/src/App.js
@@ -1,11 +1,30 @@
 import Header from "./components/header";
 import ListarTarefa from "./pages/tarefa/ListarTarefa";
+import Login from "./pages/Login";
+import DataGridDemo from "./pages/DataGrid/dataGrid";
+import ToggleColorMode from "./components/DarkTheme";
+import ActionAreaCard from "./components/card";
+import Box from '@mui/material/Box';
+import IconCheckboxes from "./components/icons";
+
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <ListarTarefa />
+      <ToggleColorMode />
+      
+      <Login/>
+      <Box sx={{ display: 'flex', gap: 2 }}>
+        <ActionAreaCard />
+        <ActionAreaCard />
+        <ActionAreaCard />
+        <ActionAreaCard />
+
+      </Box>
+      <IconCheckboxes/>
+      
+      
     </div>
   );
 }
